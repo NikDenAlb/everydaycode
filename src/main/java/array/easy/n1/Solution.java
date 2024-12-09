9bbb6508399bdfc2d90b9c61f359ed80bba4ac75
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-09 11:53:52 +0300

    n1s2 twoSumHash solution from video

--- src/main/java/array/easy/n1/Solution.java
+++ src/main/java/array/easy/n1/Solution.java
@@ -1,5 +1,7 @@
 package array.easy.n1;
 
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -8,6 +10,18 @@ class Solution {
                     return new int[]{i, j};
                 }
             }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public int[] twoSumHash(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x)) {
+                return new int[]{i, map.get(x)};
+            }
+            map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
     }

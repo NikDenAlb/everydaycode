40580d7c43b57cffc09db44f8794adb47c9b41a6
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-14 15:51:06 +0300

    org.postgresql:postgresql org.xerial:sqlite-jdbc

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.postgresql:postgresql:42.7.2")
+    testImplementation("org.xerial:sqlite-jdbc:3.47.1.0")
 }
 
 tasks.test {

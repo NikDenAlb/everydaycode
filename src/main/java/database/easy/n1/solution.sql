4ea7ff6979ec345e0fa0614e77150d76cd8d965d
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-16 13:44:27 +0300

    database/easy/n1/Solution Approach 2

--- src/main/java/database/easy/n1/solution.sql
+++ src/main/java/database/easy/n1/solution.sql
@@ -4,22 +4,22 @@ FROM Visits v
          LEFT JOIN Transactions t ON v.visit_id = t.visit_id
 WHERE t.visit_id IS NULL
 GROUP BY v.customer_id
-ORDER BY count_no_trans
+ORDER BY count_no_trans;
 
 --Editorial solutions
 
 --Approach 1: Removing Records Using NOT IN/EXISTS
-SELECT
-    customer_id,
-    COUNT(visit_id) AS count_no_trans
-FROM
-    Visits
-WHERE
-    visit_id NOT IN (
-        SELECT
-            visit_id
-        FROM
-            Transactions
-    )
-GROUP BY
-    customer_id
+SELECT customer_id,
+       COUNT(visit_id) AS count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT visit_id
+                       FROM Transactions)
+GROUP BY customer_id;
+
+--Approach 2: Removing Records Using LEFT JOIN and IS NULL
+SELECT customer_id,
+       COUNT(*) AS count_no_trans
+FROM Visits AS v
+         LEFT JOIN Transactions AS t ON v.visit_id = t.visit_id
+WHERE t.visit_id IS NULL
+GROUP BY customer_id;

f7094bcf5ea83f77d281cb56a321b397a7d7e8ef
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-18 14:03:57 +0300

    array/easy/n3/HashSetBeautySolution

---
+++ src/main/java/array/easy/n3/HashSetBeautySolution.java
@@ -0,0 +1,22 @@
+package array.easy.n3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * same but beautiful HashSet Solution
+ */
+public class HashSetBeautySolution implements Solution {
+    public boolean checkValid(int[][] matrix) {
+        for (int r = 0, n = matrix.length; r < n; ++r) {
+            Set<Integer> row = new HashSet<>();
+            Set<Integer> col = new HashSet<>();
+            for (int c = 0; c < n; ++c) {
+                if (!row.add(matrix[r][c]) || !col.add(matrix[c][r])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

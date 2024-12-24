25b5426ba3a836fdd5660bc118b9a01deea9abe6
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-24 17:56:04 +0300

    string/easy/n1/TwoPointerSolution.java

---
+++ src/main/java/string/easy/n1/TwoPointerSolution.java
@@ -0,0 +1,21 @@
+package string.easy.n1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoPointerSolution implements Solution {
+    public List<List<Integer>> largeGroupPositions(String S) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int i = 0, N = S.length(); // i is the start of each group
+        for (int j = 0; j < N; ++j) {
+            if (j == N - 1 || S.charAt(j) != S.charAt(j + 1)) {
+                // Here, [i, j] represents a group.
+                if (j - i + 1 >= 3)
+                    ans.add(Arrays.asList(i, j));
+                i = j + 1;
+            }
+        }
+        return ans;
+    }
+}

ad52a79c0c5200e6a590cb4e2bdf274177990898
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2024-12-31 19:05:12 +0300

    string/easy/n2/EasySolution.java

--- src/main/java/string/easy/n2/EasySolution.java
+++ src/main/java/string/easy/n2/EasySolution.java
@@ -1,7 +1,25 @@
 package string.easy.n2;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class EasySolution implements Solution {
     public boolean wordPattern(String pattern, String s) {
-        return false;
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) return false;
+
+        Map<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (!map.containsKey(pattern.charAt(i))) {
+                map.put(pattern.charAt(i), words[i]);
+            } else {
+                if (!map.get(pattern.charAt(i)).equals(words[i])) return false;
+            }
+        }
+
+        return new HashSet<>(map.values()).size() == map.size();
     }
 }

059ce4832d5f7807d3798f10babbf10e7bb11f7c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-03 22:01:35 +0300

    yandex/freetry/taskg/EasySolutionConsole.java

---
+++ src/main/java/yandex/freetry/taskg/EasySolutionConsole.java
@@ -0,0 +1,26 @@
+package yandex.freetry.taskg;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class EasySolutionConsole {
+    public static void main(String[] args) throws Exception {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        String J = r.readLine();
+        String S = r.readLine();
+
+        System.out.println(stoneJewels(J,S));
+    }
+
+    public static int stoneJewels(String J, String S) {
+        int result = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            char ch = S.charAt(i);
+            if (J.indexOf(ch) >= 0) {
+                ++result;
+            }
+        }
+        return result;
+    }
+}

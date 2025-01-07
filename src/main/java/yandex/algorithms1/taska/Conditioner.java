4ab3fe125a4738d937135d824c3fe63b845c2648
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-07 21:12:51 +0300

    yandex/algorithms1/taska/Conditioner.java

---
+++ src/main/java/yandex/algorithms1/taska/Conditioner.java
@@ -0,0 +1,39 @@
+package yandex.algorithms1.taska;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * test - Ok
+ */
+public class Conditioner {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] nums = reader.readLine().split(" ");
+        int troom = Integer.parseInt(nums[0]);
+        int tcond = Integer.parseInt(nums[1]);
+
+        String mode = reader.readLine();
+
+        reader.close();
+
+        System.out.println(conditionerWork(troom, tcond, mode));
+    }
+
+    /**
+     * @param tRoom температура комнаты
+     * @param tCond целевая температура
+     * @param mode  режим работы
+     * @return итоговая температура
+     */
+    public static int conditionerWork(int tRoom, int tCond, String mode) {
+
+        if (((mode.equals("heat") || mode.equals("auto")) && tCond > tRoom)
+                || ((mode.equals("freeze") || mode.equals("auto")) && tCond < tRoom)) {
+            return tCond;
+        }
+
+        return tRoom;
+    }
+}

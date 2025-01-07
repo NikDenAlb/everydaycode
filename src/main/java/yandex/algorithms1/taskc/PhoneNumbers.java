ad76f09c9cbb399e8c7119e6247a2afc00f4a73b
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-07 21:14:38 +0300

    yandex/algorithms1/taskc/PhoneNumbers.java

---
+++ src/main/java/yandex/algorithms1/taskc/PhoneNumbers.java
@@ -0,0 +1,58 @@
+package yandex.algorithms1.taskc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * test - Ok
+ */
+public class PhoneNumbers {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String newNumber = reader.readLine();
+        String numA = reader.readLine();
+        String numB = reader.readLine();
+        String numC = reader.readLine();
+
+        reader.close();
+
+        System.out.println(matchesList(newNumber, numA, numB, numC));
+    }
+
+    /**
+     * @param newNumber номер, добавляемый в телефонную книгу
+     * @param numA      первый номер в памяти
+     * @param numB      второй номер в памяти
+     * @param numC      третий номер в памяти
+     * @return список совпадений добавляемого номера с номерами из памяти, разделенный переносами строк.
+     */
+    public static String matchesList(String newNumber, String numA, String numB, String numC) {
+        long newNumberL = numberToLong(newNumber);
+        long numAL = numberToLong(numA);
+        long numBL = numberToLong(numB);
+        long numCL = numberToLong(numC);
+
+        return matcherNumber(newNumberL, numAL) + '\n'
+                + matcherNumber(newNumberL, numBL) + '\n'
+                + matcherNumber(newNumberL, numCL);
+    }
+
+    private static long numberToLong(String number) {
+        number = number.replaceAll("[^0-9]", "");
+
+        long out = (Long.parseLong(number));
+
+        if (out < 1_000_00_00) {
+            out += 1_000_00_00 * 8495L;
+        } else if (number.charAt(0) == '7') {
+            out += 10_000_000_000L;
+        }
+
+        return out;
+    }
+
+    private static String matcherNumber(long newNumber, long oldNumber) {
+        return newNumber == oldNumber ? "YES" : "NO";
+    }
+}

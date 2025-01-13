5a445aff8855752917c7fe99274ff0a0b19e560c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-13 17:52:57 +0300

    yandex/algorithms1/taski/PrisonerIf.java

---
+++ src/main/java/yandex/algorithms1/taski/PrisonerIf.java
@@ -0,0 +1,42 @@
+package yandex.algorithms1.taski;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PrisonerIf {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
+
+        reader.close();
+
+        System.out.println(isBrickFit(a, b, c, d, e));
+    }
+
+    /**
+     * @param a размер стороны кирпича
+     * @param b размер стороны кирпича
+     * @param c размер стороны кирпича
+     * @param d размер стороны дыры
+     * @param e размер стороны дыры
+     * @return "YES"/"NO" можно ли кирпич просунуть в дыру
+     */
+    public static String isBrickFit(int a, int b, int c, int d, int e) {
+        if (    d >= a && e >= b ||
+                d >= b && e >= a ||
+
+                d >= a && e >= c ||
+                d >= c && e >= a ||
+
+                d >= b && e >= c ||
+                d >= c && e >= b) {
+            return "YES";
+        }
+        return "NO";
+    }
+}

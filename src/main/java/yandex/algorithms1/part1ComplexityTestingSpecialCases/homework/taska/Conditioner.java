3f6a0ed5c00b69689bf727f600af475e08f6126d
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-15 05:42:02 +0300

    taska/Conditioner test info

--- src/main/java/yandex/algorithms1/part1ComplexityTestingSpecialCases/homework/taska/Conditioner.java
+++ src/main/java/yandex/algorithms1/part1ComplexityTestingSpecialCases/homework/taska/Conditioner.java
@@ -4,21 +4,21 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- * test - Ok
+ * test - OK: 56ms  10.57Mb
  */
 public class Conditioner {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String[] nums = reader.readLine().split(" ");
-        int troom = Integer.parseInt(nums[0]);
-        int tcond = Integer.parseInt(nums[1]);
+        int tRoom = Integer.parseInt(nums[0]);
+        int tCond = Integer.parseInt(nums[1]);
 
         String mode = reader.readLine();
 
         reader.close();
 
-        System.out.println(conditionerWork(troom, tcond, mode));
+        System.out.println(conditionerWork(tRoom, tCond, mode));
     }
 
     /**

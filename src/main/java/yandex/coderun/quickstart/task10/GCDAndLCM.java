f7b16f36932976798eb9b34cf32bf57559ed248f
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-19 18:03:07 +0300

    yandex/coderun/quickstart/task10/GCDAndLCM.java

---
+++ src/main/java/yandex/coderun/quickstart/task10/GCDAndLCM.java
@@ -0,0 +1,38 @@
+package yandex.coderun.quickstart.task10;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * <a href="https://coderun.yandex.ru/selections/quickstart/problems/gcd-and-lcm">task link</a><br>
+ * test - OK
+ */
+public class GCDAndLCM {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        String[] numsString = reader.readLine().split(" ");
+        int a = Integer.parseInt(numsString[0]);
+        int b = Integer.parseInt(numsString[1]);
+
+        int gcd = a;
+        int b2 = b;
+        while (b2 != 0) {
+            int temp = b2;
+            b2 = gcd % b2;
+            gcd = temp;
+        }
+        long lcm = (long) a / gcd * b;
+
+        writer.write(gcd + " " + lcm);
+
+
+        reader.close();
+        writer.close();
+    }
+}

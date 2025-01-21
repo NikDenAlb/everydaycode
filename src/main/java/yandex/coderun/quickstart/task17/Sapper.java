ac54e954ab209db28fb93e7f5b60a714e342a6c1
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-21 09:16:09 +0300

    yandex/coderun/quickstart/task17/Sapper.java

---
+++ src/main/java/yandex/coderun/quickstart/task17/Sapper.java
@@ -0,0 +1,66 @@
+package yandex.coderun.quickstart.task17;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * <a href="https://coderun.yandex.ru/selections/quickstart/problems/sapper">task link</a><br>
+ * test - OK
+ */
+public class Sapper {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] field = reader.readLine().split(" ");
+        int n = Integer.parseInt(field[0]);
+        int m = Integer.parseInt(field[1]);
+        int k = Integer.parseInt(field[2]);
+        int[][] matrix = new int[n][m];
+        for (int i = 0; i < k; i++) {
+            String[] mine = reader.readLine().split(" ");
+            int p = Integer.parseInt(mine[0]);
+            int q = Integer.parseInt(mine[1]);
+            matrix[p - 1][q - 1] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] != -1) {
+                    int count = 0;
+                    for (int i1 = -1; i1 < 2; i1++) {
+                        for (int j1 = -1; j1 < 2; j1++) {
+                            int x = i + i1;
+                            int y = j + j1;
+                            if (x >= 0 && x < n && y >= 0 && y < m) {
+                                if (matrix[x][y] == -1) {
+                                    count++;
+                                }
+                            }
+
+
+                        }
+                    }
+                    matrix[i][j] = count;
+                }
+            }
+        }
+
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int i1 = 0; i1 < m; i1++) {
+                if (matrix[i][i1] == -1) {
+                    out.append('*').append(" ");
+                } else out.append(matrix[i][i1]).append(" ");
+            }
+            out.append("\n");
+        }
+        writer.write(out.toString());
+
+        reader.close();
+        writer.close();
+    }
+}

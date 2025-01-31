2efba8efd84ce7ccd3323bac9bbaa62521029e15
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-01-31 14:48:44 +0300

    tbank/exam300125/progexam/TaskOne.java

---
+++ src/main/java/tbank/exam300125/progexam/TaskOne.java
@@ -0,0 +1,27 @@
+package tbank.exam300125.progexam;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class TaskOne {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[] sArr = reader.readLine().toCharArray();
+
+        if (sArr[0] == 'R') {
+            writer.write("Yes");
+        } else if (sArr[1] == 'R' && sArr[2] == 'M') {
+            writer.write("Yes");
+        } else {
+            writer.write("No");
+        }
+
+        reader.close();
+        writer.close();
+    }
+}

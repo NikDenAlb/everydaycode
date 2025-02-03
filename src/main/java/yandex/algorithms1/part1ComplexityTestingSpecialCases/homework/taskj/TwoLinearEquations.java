6d80337378906eaa36c207f440589cf9928624aa
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-02-03 21:40:15 +0300

    yandex/algorithms1/part1ComplexityTestingSpecialCases/homework/taskj/TwoLinearEquations.java

--- src/main/java/yandex/algorithms1/part1ComplexityTestingSpecialCases/homework/taskj/TwoLinearEquations.java
+++ src/main/java/yandex/algorithms1/part1ComplexityTestingSpecialCases/homework/taskj/TwoLinearEquations.java
@@ -1,23 +1,17 @@
 package yandex.algorithms1.part1ComplexityTestingSpecialCases.homework.taskj;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-
-//TODO добиться прохождения тестов
-
-//(f-cx)/d = (e-ax)/b
-//b(f-cx) = d (e-ax)
-//(da-cb)x=de-bf
-
-//(e-by)/a = (f-dy)/c
-//c(e-by)= a (f-dy)
+import java.io.OutputStreamWriter;
 
 /**
- * test 10
+ * test - Ok
  */
 public class TwoLinearEquations {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         double a = Double.parseDouble(reader.readLine());
         double b = Double.parseDouble(reader.readLine());
@@ -26,57 +20,49 @@ public class TwoLinearEquations {
         double e = Double.parseDouble(reader.readLine());
         double f = Double.parseDouble(reader.readLine());
 
-        reader.close();
+        writer.write(solveTwoLinearEquations(a, b, c, d, e, f));
 
-        System.out.println(solveTwoLinearEquations(a, b, c, d, e, f));
+        reader.close();
+        writer.close();
     }
 
     /**
-     * ax+by=e<br>
-     * cx+dy=f
+     * ax+by=c
      */
-    public static String solveTwoLinearEquations(double a, double b, double c, double d, double e, double f) {
-        if (a == b && c == d && e == f&&(a!=0||b!=0)) {
+    static String solveTwoLinearEquations(double a1, double b1, double a2, double b2, double c1, double c2) {
+        if (a1 == 0 && b1 == 0 && c1 == 0 && a2 == 0 && b2 == 0 && c2 == 0) {
             return "5";
-        }
-
-        if (a == c && a == 0 && e / b == f / d) {
-            double y = e / b;
-            return "4 " + y;
-        }
-
-        if (b == d && b == 0 && e / a == f / c) {
-            double x = e / a;
-            return "3 " + x;
-        }
-
-        if (a == 0 && b == 0 && e != 0) {
+        } else if (a1 * b2 == b1 * a2 && a1 * c2 != c1 * a2) {
             return "0";
-        }
-
-        if (c == 0 && d == 0 && f != 0) {
+        } else if (a1 == 0 && b1 == 0 && c1 != 0) {
             return "0";
-        }
-
-        if (a / c == b / d && a / c != e / f) {
+        } else if (a2 == 0 && b2 == 0 && c2 != 0) {
             return "0";
-        }
-
-        if (a / c == b / d && a / c == e / f) {
-            //y=kx+k2
-            //y=-a/bx+e/b
-            double k1 = -a / b;
-            double k2 = e / b;
-            return "1 " + k1 + " " + k2;
-        }
-
-        if (a / c != c / d) {
-            double x = (d * e - b * f) / (d * a - c * b);
-            double y = (a * f - c * e) / (a * d - c * b);
-            return "2 " + x + " " + y;
-        }
-
-        return "smth wrong, must not be here";
+        } else if (a1 == 0 && a2 == 0 && b1 * c2 != b2 * c1) {
+            return "0";
+        } else if (b1 == 0 && b2 == 0 && a1 * c2 != c1 * a2) {
+            return "0";
+        } else if (a1 * b2 == b1 * a2 && a1 * c2 == c1 * a2) {
+            if (b1 == 0 && b2 == 0) {
+                if (a1 != 0 && a2 != 0) {
+                    return "3 " + c1 / a1;
+                } else if (a1 == 0) {
+                    return "3 " + c2 / a2;
+                } else {
+                    return "3 " + c1 / a1;
+                }
+            } else if (a1 == 0 && a2 == 0) {
+                if (b1 != 0) {
+                    return "4 " + c1 / b1;
+                } else {
+                    return "4 " + c2 / b2;
+                }
+            } else if (b1 != 0) {
+                return "1 " + -a1 / b1 + " " + c1 / b1;
+            } else {
+                return "1 " + -a2 / b2 + " " + c2 / b2;
+            }
+        } else
+            return "2 " + (c1 * b2 - b1 * c2) / (a1 * b2 - a2 * b1) + " " + (a1 * c2 - c1 * a2) / (a1 * b2 - a2 * b1);
     }
 }
-

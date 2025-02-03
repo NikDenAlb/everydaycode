899677dc0a76d8dc379835e4cdfac4d1200d6fb0
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-02-03 22:26:33 +0300

    yandex/algorithms1/part2LinearSearch/homework/taskf/SymmetricSequence.java

---
+++ src/main/java/yandex/algorithms1/part2LinearSearch/homework/taskf/SymmetricSequence.java
@@ -0,0 +1,62 @@
+package yandex.algorithms1.part2LinearSearch.homework.taskf;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * test - OK
+ */
+public class SymmetricSequence {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(reader.readLine());
+        int[] nums = new int[n];
+        String[] sNums = reader.readLine().split(" ");
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(sNums[i]);
+        }
+
+        int[] result = makeSymmetricSequence(nums);
+        if (result.length == 0) {
+            writer.write('0');
+        } else {
+            StringBuilder out = new StringBuilder();
+            out.append(result.length).append('\n');
+            for (int i = 0; i < result.length - 1; i++) {
+                out.append(result[i]).append(' ');
+            }
+            out.append(result[result.length - 1]);
+            writer.write(out.toString());
+        }
+
+        reader.close();
+        writer.close();
+    }
+
+    public static int[] makeSymmetricSequence(int[] nums) {
+        int outLen = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            boolean isSymmetric = true;
+            for (int j = i; nums.length - 1 + i - j > j; j++) {
+                if (nums[nums.length - 1 + i - j] != nums[j]) {
+                    isSymmetric = false;
+                    break;
+                }
+            }
+            if (isSymmetric) {
+                outLen = i;
+                break;
+            }
+        }
+        int[] out = new int[outLen];
+        for (int i = 0; i < outLen; i++) {
+            out[i] = nums[outLen - 1 - i];
+        }
+        return out;
+    }
+}

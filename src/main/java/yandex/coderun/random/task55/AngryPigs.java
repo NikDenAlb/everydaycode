1175725fa121ece9e1ba18e7269c9ac42311025b
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-02-06 19:50:30 +0300

    yandex/coderun/random/task55/AngryPigs.java

---
+++ src/main/java/yandex/coderun/random/task55/AngryPigs.java
@@ -0,0 +1,31 @@
+package yandex.coderun.random.task55;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <a href="https://coderun.yandex.ru/problem/angry-pigs">task link</a><br>
+ * test - OK
+ */
+public class AngryPigs {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(reader.readLine());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(reader.readLine().split(" ")[0]));
+        }
+
+        writer.write(String.valueOf(set.size()));
+
+        reader.close();
+        writer.close();
+    }
+}

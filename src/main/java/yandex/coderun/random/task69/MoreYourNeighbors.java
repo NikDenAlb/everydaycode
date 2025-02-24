16d2bf2aa72abd13c18d60445364e585b99367a5
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-02-24 05:07:26 +0300

    yandex/coderun/random/task69/MoreYourNeighbors.java

---
+++ src/main/java/yandex/coderun/random/task69/MoreYourNeighbors.java
@@ -0,0 +1,36 @@
+package yandex.coderun.random.task69;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+/**
+ * <a href="https://coderun.yandex.ru/problem/more-your-neighbors">task link</a><br>
+ * test - OK
+ */
+public class MoreYourNeighbors {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] nums = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int count = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (nums[i] > nums[i - 1]) {
+                    count++;
+                }
+                i++;
+            }
+        }
+
+        writer.write(String.valueOf(count));
+
+        reader.close();
+        writer.close();
+    }
+}

46c924cd172b6b7b407f74c9ddb876509a18e799
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-02-27 09:55:16 +0300

    yandex/coderun/random/task59/NumberWordsText.java

---
+++ src/main/java/yandex/coderun/random/task59/NumberWordsText.java
@@ -0,0 +1,32 @@
+package yandex.coderun.random.task59;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <a href="https://coderun.yandex.ru/problem/number-words-text">task link</a><br>
+ * test - OK
+ */
+public class NumberWordsText {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Set<String> words = new HashSet<>();
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            words.addAll(List.of(line.split(" ")));
+        }
+
+        writer.write(String.valueOf(words.size()));
+
+        reader.close();
+        writer.close();
+    }
+}

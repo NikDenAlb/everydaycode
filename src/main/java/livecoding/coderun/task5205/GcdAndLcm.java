34737d7203d1dfedf2513f9e8d9ff02590ce6a34
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-03-02 17:37:41 +0300

    livecoding/coderun/task5205/GcdAndLcm.java

---
+++ src/main/java/livecoding/coderun/task5205/GcdAndLcm.java
@@ -0,0 +1,54 @@
+package livecoding.coderun.task5205;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+4, 15
+----
+---- ---- ---- ---
+
+---- ---- ---- ---
+----
+
+----
+---
+
+---
+-
+
+-
+0
+*/
+/**
+ * <a href="https://coderun.yandex.ru/problem/gcd-and-lcm">task link</a><br>
+ * <https://youtu.be/sT0ky_U2cQo">video link</a>
+ */
+public class GcdAndLcm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] line = reader.readLine().split(" ");
+        int a = Integer.parseInt(line[0]);
+        int b = Integer.parseInt(line[1]);
+
+        int gcd = a;
+        int bC = b;
+        while (bC != 0) {
+            int temp = gcd;
+            gcd = bC;
+            bC = temp % bC;
+        }
+
+        long lcm = (long) a * b / gcd;
+
+        writer.write(gcd + " " + lcm);
+
+        reader.close();
+        writer.close();
+    }
+}

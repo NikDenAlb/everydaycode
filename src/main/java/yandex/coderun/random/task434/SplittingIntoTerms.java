29b18a9669b41427a1e486feb04268962e479d44
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-03-17 17:55:25 +0300

    yandex/coderun/random/task434/SplittingIntoTerms.java

--- src/main/java/yandex/coderun/random/task434/SplittingIntoTerms.java
+++ src/main/java/yandex/coderun/random/task434/SplittingIntoTerms.java
@@ -1,58 +1,73 @@
 package yandex.coderun.random.task434;
-
+//TODO
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-
-//TODO 4+4+2+1 need to add
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class SplittingIntoTerms {
     public static void main(String[] args) throws IOException {
-        String inputtxt = "src/main/java/yandex/coderun/random/task434/input.txt";
-        String outputtxt = "src/main/java/yandex/coderun/random/task434/output.txt";
-
-        BufferedReader reader = new BufferedReader(new FileReader(inputtxt));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outputtxt));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(reader.readLine());
         StringBuilder out = new StringBuilder();
 
-        int[] terms = new int[n];
-        Arrays.fill(terms, 1);
-        int length = n;
+        printAllUniqueParts(n, out);
 
-        do {
-            for (int i = 0; i < length; i++) {
-                out.append(terms[i]).append(" + ");
-            }
-            out.setLength(out.length() - 3);
-            out.append('\n');
-            boolean flag = false;
-            for (int i = length - 2; i > 0; i--) {
-                if (terms[i - 1] > terms[i]) {
-                    terms[i]++;
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                terms[0]++;
-            }
-            terms[length - 1]--;
-            if (terms[length - 1] == 0) {
-                length--;
-            }
-        } while (terms[0] < n);
-        if (n != 1) {
-            out.append(n);
+        String[] lines = out.toString().split("\n");
+        out = new StringBuilder();
+        for (int i = lines.length - 1; i > 0; i--) {
+            out.append(lines[i]).append("\n");
         }
+        out.append(lines[0]);
 
         writer.write(out.toString());
 
         reader.close();
         writer.close();
+    }
+
+    static void printArray(int[] p, int n, StringBuilder out) {
+        for (int i = 0; i < n; i++) {
+            out.append(p[i]).append(" + ");
+        }
+        out.setLength(out.length() - 3);
+        out.append("\n");
+    }
+
+    static void printAllUniqueParts(int n, StringBuilder out) {
+        int[] p = new int[n];
+        int k = 0;
+        p[k] = n;
+
+        while (true) {
+
+            printArray(p, k + 1, out);
+
+            int rem_val = 0;
+            while (k >= 0 && p[k] == 1) {
+                rem_val += p[k];
+                k--;
+            }
+
+            if (k < 0) {
+                out.setLength(out.length() - 1);
+                return;
+            }
+
+            p[k]--;
+            rem_val++;
+
+            while (rem_val > p[k]) {
+                p[k + 1] = p[k];
+                rem_val = rem_val - p[k];
+                k++;
+            }
+
+            p[k + 1] = rem_val;
+            k++;
+        }
     }
 }

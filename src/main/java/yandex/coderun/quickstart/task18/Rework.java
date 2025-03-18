e2bab23c9fd445b9a1a259dbb70f954767c480a7
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-03-18 21:51:00 +0300

    yandex/coderun/quickstart/task18/Rework.java finish

--- src/main/java/yandex/coderun/quickstart/task18/Rework.java
+++ src/main/java/yandex/coderun/quickstart/task18/Rework.java
@@ -5,13 +5,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-//TODO Morning
 
 public class Rework {
     public static void main(String[] args) throws IOException {
@@ -19,33 +18,34 @@ public class Rework {
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] stringNM = reader.readLine().split(" ");
-        int n = Integer.parseInt(stringNM[0]);
         int m = Integer.parseInt(stringNM[1]);
 
-        List<Set<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new HashSet<>());
-        }
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        graph.put(1, new HashSet<>(List.of(1)));
         for (int i = 0; i < m; i++) {
             String[] string = reader.readLine().split(" ");
-            int a = Integer.parseInt(string[0]) - 1;
-            int b = Integer.parseInt(string[1]) - 1;
+            int a = Integer.parseInt(string[0]);
+            int b = Integer.parseInt(string[1]);
+            graph.putIfAbsent(a, new HashSet<>());
+            graph.putIfAbsent(b, new HashSet<>());
             graph.get(a).add(b);
             graph.get(b).add(a);
         }
 
-        Set<Integer> connV1 = new HashSet<>(List.of(0));
-        Set<Integer> newConn = new HashSet<>(List.of(0));
-        Set<Integer> addConn = newConn.stream().flatMap(e -> graph.get(e).stream()).collect(Collectors.toSet());
+        Set<Integer> connV1 = new HashSet<>();
+        Set<Integer> newConn = new HashSet<>();
+        newConn.add(1);
         while (!newConn.isEmpty()) {
+            Set<Integer> addConn = newConn.stream().flatMap(e -> graph.get(e).stream()).collect(Collectors.toSet());
             addConn.removeAll(connV1);
             connV1.addAll(addConn);
-            newConn = new HashSet<>(addConn);
+            newConn = addConn;
         }
+
         StringBuilder out = new StringBuilder();
         out.append(connV1.size()).append("\n");
         if (!connV1.isEmpty()) {
-            connV1.stream().sorted().forEach(e -> out.append(e + 1).append(" "));
+            connV1.stream().sorted().forEach(e -> out.append(e).append(" "));
         }
 
         writer.write(out.toString());
@@ -53,5 +53,4 @@ public class Rework {
         reader.close();
         writer.close();
     }
-
 }

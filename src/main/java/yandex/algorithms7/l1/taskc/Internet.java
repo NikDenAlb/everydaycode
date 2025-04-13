328379da5084ca8ed7e1f53d60fbe3a1af29b50c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-04-13 23:31:46 +0300

    yandex/algorithms7/l1/taskc/Internet.java comments

--- src/main/java/yandex/algorithms7/l1/taskc/Internet.java
+++ src/main/java/yandex/algorithms7/l1/taskc/Internet.java
@@ -13,25 +13,35 @@ public class Internet {
         int m = Integer.parseInt(reader.readLine());
         int[] a = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
+        //Массив со значениями максимального времени, которое можно купить за стоимость каждой карточки
         int[] pP = new int[31];
         pP[0] = a[0];
-        for (int i = 1; i < pP.length; i++) {
-            pP[i] = Math.max(pP[i - 1] << 1, a[i]);
-        }
 
+    /*
+        Если найдётся индекс X, при котором за стоимость следующей по цене карточки, можно купить нужное время
+        карточками в интервале [0, X], то заполнять массив дальше нет необходимости.
+    */
         int index = 0;
         while (pP[index] << 1 < m && index < pP.length - 1) {
             index++;
+            pP[index] = Math.max(pP[index - 1] << 1, a[index]);
         }
 
         int out = 0;
         do {
+        /*
+            На индексе 0 возможный остаток от деления нужно будет закрыть ещё одной карточкой 0. Сам же
+            результат деления на pP[0] может быть либо 0, либо 1. Другие варианты невозможны, так как большее значение
+            привело бы к старшему индексу.
+        */
             if (index == 0) {
-                out += (((m + pP[index] - 1) / pP[index]) << index);
+                out += m > pP[index] ? 2 : 1;
                 break;
             }
+            //Максимально эффективно складываем суммы для времени, кратного pP[index]. Затем отбрасываем целую часть
             out += (m / pP[index]) << index;
-            m = m % pP[index];
+            m %= pP[index];
+            //Смотрим, нужно ли разбивать остаток младшими индексами или же выгоднее взять ещё одну pP[index]
             if (m >= pP[index - 1] << 1) {
                 out += 1 << index;
                 break;

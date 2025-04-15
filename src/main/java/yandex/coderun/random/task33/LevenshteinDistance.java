157f22872889dd8edd333805377a9f44da671974
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-04-15 23:10:58 +0300

    yandex/coderun/random/task33/LevenshteinDistance.java

---
+++ src/main/java/yandex/coderun/random/task33/LevenshteinDistance.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class LevenshteinDistance {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str1 = reader.readLine();
+        String str2 = reader.readLine();
+
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(
+                                    dp[i - 1][j] + 1,
+                                    dp[i][j - 1] + 1),
+                            dp[i - 1][j - 1] + 1
+                    );
+                }
+            }
+        }
+
+        writer.write(String.valueOf(dp[dp.length - 1][dp[dp.length - 1].length - 1]));
+
+        reader.close();
+        writer.close();
+    }
+}

6469ad3d364f5a10746c5516213c52eaaad4cfe6
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-15 20:18:58 +0300

    leetcode/random/task13/Roman to Integer

---
+++ src/main/java/leetcode/random/task13/EasySolution.java
@@ -0,0 +1,24 @@
+package leetcode.random.task13;
+
+class EasySolution {
+    int romanToInt(String s) {
+        int ans = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int num = switch (s.charAt(i)) {
+                case 'I' -> 1;
+                case 'V' -> 5;
+                case 'X' -> 10;
+                case 'L' -> 50;
+                case 'C' -> 100;
+                case 'D' -> 500;
+                case 'M' -> 1000;
+                default -> throw new Error("invalid input");
+            };
+            if (3 * num < ans)
+                ans -= num;
+            else
+                ans += num;
+        }
+        return ans;
+    }
+}

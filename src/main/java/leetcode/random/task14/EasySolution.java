1d65617fbf2e3a46b895938a8b247726d01053e8
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-16 21:04:21 +0300

    leetcode/random/task14/Longest Common Prefix

---
+++ src/main/java/leetcode/random/task14/EasySolution.java
@@ -0,0 +1,17 @@
+package leetcode.random.task14;
+
+class EasySolution {
+    String longestCommonPrefix(String[] strs) {
+        if (strs[0].isEmpty()) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].length() == i || strs[j].charAt(i) != strs[0].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}

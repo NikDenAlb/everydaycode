daa91fb07fd3baba5acd5b353b2579e2d13d7660
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-20 19:30:28 +0300

    leetcode/random/task26/Remove Duplicates from Sorted Array

---
+++ src/main/java/leetcode/random/task26/EasySolution.java
@@ -0,0 +1,13 @@
+package leetcode.random.task26;
+
+class EasySolution {
+    int removeDuplicates(int[] nums) {
+        int j = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+}

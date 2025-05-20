9fa9a7e0e0777b18ee44e8096f82e9a33f1e5166
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-20 18:30:10 +0300

    leetcode/random/task283/Move Zeroes

---
+++ src/main/java/leetcode/random/task283/EasySolution.java
@@ -0,0 +1,16 @@
+package leetcode.random.task283;
+
+class EasySolution {
+    void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                continue;
+            }
+            nums[j++] = nums[i];
+        }
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}

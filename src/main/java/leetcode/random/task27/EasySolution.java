7765ab85653264be1e350128e46b985ae84f5194
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-21 15:17:44 +0300

    leetcode/random/task27/Remove Element

---
+++ src/main/java/leetcode/random/task27/EasySolution.java
@@ -0,0 +1,13 @@
+package leetcode.random.task27;
+
+class EasySolution {
+    int removeElement(int[] nums, int val) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+}

12af74b9bb4b85c1128a3596c6abe89365560884
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-23 17:32:36 +0300

    leetcode/random/task392/Is Subsequence

---
+++ src/main/java/leetcode/random/task392/EasySolution.java
@@ -0,0 +1,13 @@
+package leetcode.random.task392;
+
+class EasySolution {
+    boolean isSubsequence(String s, String t) {
+        int j = 0;
+        for (int i = 0; i < t.length() && j < s.length(); i++) {
+            if (t.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+        }
+        return j == s.length();
+    }
+}

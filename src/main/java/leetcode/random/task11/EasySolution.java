247bf393e925b93f6bdeb58c4a4519c6098d41d5
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-05-26 09:56:20 +0300

    leetcode/random/task11/Container With Most Water

---
+++ src/main/java/leetcode/random/task11/EasySolution.java
@@ -0,0 +1,16 @@
+package leetcode.random.task11;
+
+class EasySolution {
+    int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            max = Math.max(max, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
+}

e3d878f0e0895266b407b7e848ea4c0d28cee090
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-06-14 12:30:05 +0300

    leetcode/random/task104/Maximum Depth of Binary Tree

---
+++ src/main/java/leetcode/random/task104/EasySolution.java
@@ -0,0 +1,29 @@
+package leetcode.random.task104;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class EasySolution {
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}

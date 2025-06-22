d3013379d13aebbd811197560615f1eab54005c5
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-06-22 20:26:18 +0300

    leetcode/random/task1137/N-th Tribonacci Number

---
+++ src/main/java/leetcode/random/task1137/EasySolution.java
@@ -0,0 +1,19 @@
+package leetcode.random.task1137;
+
+public class EasySolution {
+    int tribonacci(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int[] dp = new int[++n];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n - 1];
+    }
+}

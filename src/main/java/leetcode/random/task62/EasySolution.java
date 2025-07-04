00b790815ff07dce704a6d6bd645de69c2e413d8
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-07-04 22:00:57 +0300

    leetcode/random/task62/Unique Paths

---
+++ src/main/java/leetcode/random/task62/EasySolution.java
@@ -0,0 +1,17 @@
+package leetcode.random.task62;
+
+import java.util.Arrays;
+
+class EasySolution {
+    int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        Arrays.fill(dp[0], 1);
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}

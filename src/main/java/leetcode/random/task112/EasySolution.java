66db0b982d5be4d39d2574dee065911bfb9bab07
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-08-03 11:45:03 +0300

    leetcode/random/task112/Path Sum

---
+++ src/main/java/leetcode/random/task112/EasySolution.java
@@ -0,0 +1,13 @@
+package leetcode.random.task112;
+
+class EasySolution {
+    boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        return (hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val));
+    }
+}

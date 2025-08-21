4f3ddb6a5438640d70323aedafaef63b6f34a04f
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-08-21 22:12:58 +0300

    leetcode/random/task55/Jump Game

---
+++ src/main/java/leetcode/random/task55/EasySolution.java
@@ -0,0 +1,14 @@
+package leetcode.random.task55;
+
+class EasySolution {
+    boolean canJump(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (i > max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+        }
+        return true;
+    }
+}

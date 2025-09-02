957b5ced9c65a1fc66619764f0983cd928b5ec40
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-09-02 20:41:10 +0300

    leetcode/random/task680/Valid Palindrome II

---
+++ src/main/java/leetcode/random/task680/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.random.task680;
+
+class Solution {
+    boolean validPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return (isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1));
+            }
+        }
+        return true;
+    }
+    boolean isPalindrome(String s, int l, int r) {
+        for ( ; l < r; l++, r--) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

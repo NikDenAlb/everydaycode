ea7e022117a99dfc6984a6d2f7e8eef0b632488c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-09-08 21:42:03 +0300

    learning/sorokin/multithreading/runnable

--- src/main/java/learning/sorokin/multithreading/base/Main.java
+++ src/main/java/learning/sorokin/multithreading/base/Main.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         MyThread thread1 = new MyThread(1);
         MyThread thread2 = new MyThread(2);
+
+        System.out.println("---2Threads start()---");
         thread1.start();
         thread2.start();
 
         Thread.sleep(5000);
-        System.out.println("---Waited 5000---");
 
+        System.out.println("---2Threads run()---");
         thread1.run();
         thread2.run();
 
@@ -18,6 +20,14 @@ public class Main {
         for (int i = 0; i < 5; i++) {
             MyThread thread = new MyThread(i);
             thread.start();
+        }
+
+        Thread.sleep(5000);
+
+        System.out.println("---5Runnables with broken id == 3:---");
+        for (int i = 0; i < 5; i++) {
+            Runnable runnable = new MyRunnable(i);
+            new Thread(runnable).start();
         }
     }
 }
---
+++ src/main/java/learning/sorokin/multithreading/base/MyRunnable.java
@@ -0,0 +1,24 @@
+package learning.sorokin.multithreading.base;
+
+public class MyRunnable implements Runnable {
+    private final int threadId;
+
+    public MyRunnable(int threadId) {
+        this.threadId = threadId;
+    }
+
+    @Override
+    public void run() {
+        if (threadId == 3) {
+            throw  new RuntimeException();
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i + ": from thread with runnable #" + threadId);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}

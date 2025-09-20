ea3b3cab1626a0a80e0ec9bfb4c8c00eb208d6f5
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-09-20 19:33:14 +0300

    yandex/coderun/random/task445/Tiles prep

---
+++ src/main/java/yandex/coderun/random/task445/Tiles.java
@@ -0,0 +1,23 @@
+package yandex.coderun.random.task445;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * <a href="https://coderun.yandex.ru/problem/tiles">task link</a><br>
+ * test - ?
+ */
+public class Tiles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+        reader.close();
+        writer.close();
+    }
+}

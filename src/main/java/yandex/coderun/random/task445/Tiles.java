f4bd2c3f73be173c45b7c3c5dbfded828cbe112c
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-09-21 16:18:31 +0300

    yandex/coderun/random/task445/Tiles.java

--- src/main/java/yandex/coderun/random/task445/Tiles.java
+++ src/main/java/yandex/coderun/random/task445/Tiles.java
@@ -8,16 +8,34 @@ import java.io.OutputStreamWriter;
 
 /**
  * <a href="https://coderun.yandex.ru/problem/tiles">task link</a><br>
- * test - ?
+ * test - OK
  */
 public class Tiles {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String[] input = reader.readLine().split(" ");
+        int B = Integer.parseInt(input[0]);
+        int W = Integer.parseInt(input[1]);
 
+        int hP = (B + 4) / 2;
 
-        reader.close();
-        writer.close();
+        for (int l = 3, r = hP / 2; l <= r; ) {
+            int m = (l + r) / 2;
+            int w = (m - 2) * (hP - m - 2);
+            if (w < W) {
+                l = m + 1;
+            } else if (w > W) {
+                r = m - 1;
+            } else {
+                writer.write(hP - m + " " + m);
+                reader.close();
+                writer.close();
+                return;
+            }
+        }
+
+        throw new RuntimeException();
     }
 }

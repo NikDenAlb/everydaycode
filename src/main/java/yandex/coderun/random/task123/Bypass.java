881c275048cf1186db75415cc398829208b2d30a
nikdenalb <136702482+NikDenAlb@users.noreply.github.com>
2025-09-27 13:23:49 +0300

    sunday coderun prep

---
+++ src/main/java/yandex/coderun/random/task123/Bypass.java
@@ -0,0 +1,22 @@
+package yandex.coderun.random.task123;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * <a href="https://coderun.yandex.ru/problem/bypass">task link</a><br>
+ * test - ??
+ */
+public class Bypass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        reader.close();
+        writer.close();
+    }
+}
